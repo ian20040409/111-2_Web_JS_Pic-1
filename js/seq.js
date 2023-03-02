391604839aee949dce55cbfa6ea4034c395efa1a
LNYou <78723629+ian20040409@users.noreply.github.com>
2023-03-02 11:33:49 +0800

    Add files via upload

--- js/seq.js
+++ js/seq.js
@@ -1,13 +1,14 @@
 //循序播放陣列中指定的圖檔
-var jslmg = new Array("images/OOl.jpg","images/OO2.jpg","images/OO3.jpg","images/OO4.jpg","images/OO5.jpg",)
-var jslmg 1en = jslmg.length; //圖檔量
+var jsImg = new Array("images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg");
+var jsImg_len = jsImg.length; //圖檔量
 
-var i=0;
+var i = 1;
 
 //設定每兩秒執行一次 sequent 主 此行要在 function 之外
-setInterval ( "sequImg ( )",2000);
-function sequImg() {  //循序播放
-    document.getElementById("my_div").innerHTML = "<img src='"+jsImg[i]+"' width: 1140px>";
-        i++;
-        if (i>=jsTmg_len) i=0;
+setInterval("sequImg()", 2000);
+//循序播放
+function sequImg() {  
+    document.getElementById("my_div").innerHTML = "<img src='" + jsImg[i] + "' width:1140px>";
+    i++;
+    if (i >= jsImg_len) i = 0;
 }
